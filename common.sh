f24fbf01a8f9d919560875c7e68a3f79367df8e6
Doug Hellmann <doug@doughellmann.com>
2019-01-25 22:32:14 -0500

    log scripts automatically
    Rather than forcing users to remember to capture the script output,
    do it automatically.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- common.sh
+++ common.sh
@@ -10,3 +10,13 @@ export RHCOS_IMAGE_NAME="redhat-coreos-maipo-${RHCOS_IMAGE_VERSION}"
 # config drive support, or perhaps a completely new image?
 #export RHCOS_IMAGE_FILENAME="${RHCOS_IMAGE_NAME}-openstack.qcow2"
 export RHCOS_IMAGE_FILENAME="${RHCOS_IMAGE_NAME}-qemu.qcow2"
+
+# Log output automatically
+LOGDIR="$(dirname $0)/logs"
+if [ ! -d "$LOGDIR" ]; then
+    mkdir -p "$LOGDIR"
+fi
+LOGFILE="$LOGDIR/$(basename $0 .sh)-$(date +%F-%H%M%S).log"
+echo "Logging to $LOGFILE"
+# Set fd 1 and 2 to write to the log file
+exec 1>"${LOGFILE}" 2>&1
