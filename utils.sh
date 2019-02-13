c3c8ea9a5c7f03ce78f833204420d4b3fed9cd6f
Antoni Segura Puimedon <antoni@redhat.com>
2019-02-13 11:50:28 +0100

    Add wait_for_json to wait for ironic interaction

--- utils.sh
+++ utils.sh
@@ -52,3 +52,33 @@ function domain_net_ip() {
 
     net_iface_dhcp_ip "$net" "$hwaddr"
 }
+
+function wait_for_json() {
+    local name
+    local url
+    local curl_opts
+    local timeout
+
+    local start_time
+    local curr_time
+    local time_diff
+
+    name="$1"
+    url="$2"
+    timeout="$3"
+    shift 3
+    curl_opts="$@"
+    echo -n "Waiting for $name to respond"
+    start_time=$(date +%s)
+    until curl -g -X GET "$url" "${curl_opts[@]}" 2> /dev/null | jq '.' 2> /dev/null > /dev/null; do
+        echo -n "."
+        curr_time=$(date +%s)
+        time_diff=$(($curr_time - $start_time))
+        if [[ $time_diff -gt $timeout ]]; then
+            echo "\nTimed out waiting for $name"
+            return 1
+        fi
+    done
+    echo " Success!"
+    return 0
+}
