33966536d8633305eed343a219017e21e81a9f72
Mark McLoughlin <markmc@redhat.com>
2019-02-21 13:08:29 +0000

    Start using kni-install
    Use kni-install to launch the bootstrap VM and generate the ignition
    configs for the masters.

--- 05_build_ocp_installer.sh
+++ 05_build_ocp_installer.sh
@@ -9,7 +9,7 @@ figlet "Building the Installer" | lolcat
 eval "$(go env)"
 echo "$GOPATH" | lolcat # should print $HOME/go or something like that
 
-pushd "$GOPATH/src/github.com/openshift/installer"
+pushd "$GOPATH/src/github.com/metalkube/kni-installer"
 export MODE=release
 export TAGS=libvirt
 ./hack/build.sh
--- 06_deploy_bootstrap_vm.sh
+++ 06_deploy_bootstrap_vm.sh
@@ -6,62 +6,20 @@ source ocp_install_env.sh
 source common.sh
 source utils.sh
 
-# FIXME this is configuring for the libvirt backend which is dev-only ref
-# https://github.com/openshift/installer/blob/master/docs/dev/libvirt-howto.md
-# We may need some additional steps from that doc in 02* and also to make the
-# qemu endpoint configurable?
 if [ ! -d ocp ]; then
     mkdir -p ocp
-    cat > ocp/install-config.yaml << EOF
-apiVersion: v1beta3
-baseDomain: ${BASE_DOMAIN}
-metadata:
-  name: ${CLUSTER_NAME}
-platform:
-  libvirt: {}
-pullSecret: |
-  ${PULL_SECRET}
-sshKey: |
-  ${SSH_PUB_KEY}
-EOF
+    generate_ocp_install_config ocp
 fi
 
+# We are only doing this to generate the master ignition
+# configs for patching later. This will go away when
+# "create cluster" also launches the masters
+create_ignition_configs ocp
+cp ocp/master.ign ocp/master.ign.tmp
 
-$GOPATH/src/github.com/openshift/installer/bin/openshift-install --dir ocp --log-level=debug create ignition-configs
-
-# Here we can make any necessary changes to the ignition configs/manifests
-# they can later be sync'd back into the installer via a new baremetal target
-
-# Now re create the cluster (using the existing install-config and ignition-configs)
-# Since we set the replicas to null, we should only get the bootstrap VM
-# FIXME(shardy) this doesn't work, it creates the bootstrap and one master
-#$GOPATH/src/github.com/openshift/installer/bin/openshift-install --dir ocp --log-level=debug create cluster
-#exit 1
-
-# ... so for now lets create the bootstrap VM manually and use the generated ignition config
-# See https://coreos.com/os/docs/latest/booting-with-libvirt.html
-IGN_FILE="/var/lib/libvirt/images/${CLUSTER_NAME}-bootstrap.ign"
-sudo cp ocp/bootstrap.ign ${IGN_FILE}
-
-# Apply patches to bootstrap ignition
-apply_ignition_patches bootstrap "$IGN_FILE"
-
-LATEST_IMAGE=$(ls -ltr redhat-coreos-maipo-*-qemu.qcow2 | tail -n1 | awk '{print $9}')
-sudo cp $LATEST_IMAGE /var/lib/libvirt/images/${CLUSTER_NAME}-bootstrap.qcow2
-virt-install --connect qemu:///system \
-             --import \
-             --name ${CLUSTER_NAME}-bootstrap \
-             --ram 4096 --vcpus 4 \
-             --os-type=linux \
-             --os-variant=virtio26 \
-             --disk path=/var/lib/libvirt/images/${CLUSTER_NAME}-bootstrap.qcow2,format=qcow2,bus=virtio \
-             --vnc --noautoconsole \
-             --network bridge=baremetal --network bridge=provisioning \
-             --print-xml > ocp/bootstrap-vm.xml
-sed -i 's|type="kvm"|type="kvm" xmlns:qemu="http://libvirt.org/schemas/domain/qemu/1.0"|' ocp/bootstrap-vm.xml
-sed -i "/<\/devices>/a <qemu:commandline>\n  <qemu:arg value='-fw_cfg'/>\n  <qemu:arg value='name=opt/com.coreos/config,file=${IGN_FILE}'/>\n</qemu:commandline>" ocp/bootstrap-vm.xml
-virsh define ocp/bootstrap-vm.xml
-virsh start ${CLUSTER_NAME}-bootstrap
+# "create cluster" only launches the bootstrap VM only for now
+create_cluster ocp
+cp ocp/master.ign.tmp ocp/master.ign
 sleep 10
 
 while ! domain_net_ip ${CLUSTER_NAME}-bootstrap baremetal; do
