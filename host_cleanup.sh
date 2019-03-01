81cb030559a3b32555b784a69345ff2b981b4300
Bob Fournier <bfournie@redhat.com>
2019-03-01 08:41:36 -0500

    Create a pod for ironic and add Ironic containers to it

--- host_cleanup.sh
+++ host_cleanup.sh
@@ -3,11 +3,16 @@ set -x
 
 source common.sh
 
-# Kill and remove the running ironic
+# Kill and remove the running ironic containers
 for name in ironic ironic-inspector ; do 
     sudo podman ps | grep -w "$name$" && sudo podman kill $name
     sudo podman ps --all | grep -w "$name$" && sudo podman rm $name
 done
+
+# Remove existing pod
+if  sudo podman pod exists ironic-pod ; then
+    sudo podman pod rm ironic-pod
+fi
 
 ANSIBLE_FORCE_COLOR=true ansible-playbook \
     -e "working_dir=$WORKING_DIR" \
