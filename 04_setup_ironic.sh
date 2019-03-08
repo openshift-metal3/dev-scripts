6cbba05786d1fe239f72f8c9cc9786749a6f267e
Stephen Benjamin <stephen@redhat.com>
2019-03-08 11:33:48 -0500

    Force remove Ironic containers

--- 04_setup_ironic.sh
+++ 04_setup_ironic.sh
@@ -82,12 +82,12 @@ popd
 
 for name in ironic ironic-inspector ; do 
     sudo podman ps | grep -w "$name$" && sudo podman kill $name
-    sudo podman ps --all | grep -w "$name$" && sudo podman rm $name
+    sudo podman ps --all | grep -w "$name$" && sudo podman rm $name -f
 done
 
 # Remove existing pod
 if  sudo podman pod exists ironic-pod ; then 
-    sudo podman pod rm ironic-pod
+    sudo podman pod rm ironic-pod -f
 fi
 
 # Create pod
--- host_cleanup.sh
+++ host_cleanup.sh
@@ -6,12 +6,12 @@ source common.sh
 # Kill and remove the running ironic containers
 for name in ironic ironic-inspector ; do 
     sudo podman ps | grep -w "$name$" && sudo podman kill $name
-    sudo podman ps --all | grep -w "$name$" && sudo podman rm $name
+    sudo podman ps --all | grep -w "$name$" && sudo podman rm $name -f
 done
 
 # Remove existing pod
 if  sudo podman pod exists ironic-pod ; then
-    sudo podman pod rm ironic-pod
+    sudo podman pod rm ironic-pod -f
 fi
 
 ANSIBLE_FORCE_COLOR=true ansible-playbook \
