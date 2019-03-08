5743cb4b7cde80b3ff5d4a6c21537528ca8d5054
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-03-08 16:59:29 +0100

    Use MachineConfig to deliver files to masters
    Co-authored-by: Antoni Segura Puimedon <antoni@redhat.com>

--- utils.sh
+++ utils.sh
@@ -2,165 +2,37 @@
 
 set -o pipefail
 
-function apply_ignition_patches() {
-local kind
-local target
-local target_name
-local wd
-local current
-local current_patch_name
+function generate_assets() {
+  rm -rf assets/generated && mkdir assets/generated
+  for file in $(find assets/templates -type f -printf "%P\n"); do
+      echo "Templating ${file} to assets/generated/${file}"
+      cp assets/{templates,generated}/${file}
 
-kind="$1"
-target="$2"
-wd=$(mktemp -d)
-target_name="$(basename "$target")"
-
-current="$target"
-for patch in $(ls "${PWD}/ignition_patches/${kind}/"*.json); do
-    current_patch_name="$(basename "$patch")"
-    (>&2 echo "Patching "$current" with "$patch"")
-    jsonpatch "$current" "$patch" > "${wd}/${target_name}_${current_patch_name}"
-    current="${wd}/${target_name}_${current_patch_name}"
-done
-
-# Process also generated if they exist
-for patch in $(ls "${PWD}/ignition_patches/generated/${kind}/"*.json); do
-    current_patch_name="$(basename "$patch")"
-    (>&2 echo "Patching "$current" with "$patch"")
-    jsonpatch "$current" "$patch" > "${wd}/${target_name}_${current_patch_name}"
-    current="${wd}/${target_name}_${current_patch_name}"
-done
-
-if [[ "$current" != "$target" ]]; then
-    sudo cp "$current" "$target"
-fi
-}
-
-function patch_node_ignition() {
-    local kind
-    local bootstrap
-    local wd
-    local config
-
-    kind="$1"
-    bootstrap="$2"
-    wd="$(mktemp -d)"
-
-    # Wait for the release config to be pulled
-    while
-        config=$($SSH "core@$bootstrap" \
-            sudo ls "/etc/mcs/bootstrap/machine-configs/${kind}*")
-        [ -z "$config" ]
-    do
-        (>&2 echo "Waiting 5 seconds more for $kind Release MachineConfig to be pulled...")
-        sleep 5
-    done
-
-    # Retrieve the MachineConfig
-    $SSH "core@$bootstrap" \
-        sudo cat "$config" > "${wd}/${kind}.yaml"
-
-    apply_yaml_patches "$kind" "${wd}/${kind}.yaml"
-
-    # Put it back
-    # TODO: Put a check so we only do this step if the yaml was successfully generated
-    $SSH < "${wd}/${kind}.yaml" "core@$bootstrap" sudo dd of="$config"
-}
-
-function machineconfig_generate_patches() {
-    set -x
-    local kind
-    local path
-    local value
-    local search_path
-    local config_type
-    local files_template
-    local units_template
-    local octal_mode
-    local decimal_mode
-    local dest_path
-    local ifs
-    local simple_name
-    local content
-
-    files_template=$(cat <<'EOF'
-[{"op": "add", "path": "/${path}/-", "value": {"filesystem": "root", "path": "${dest_path}", "user": {"name": "root"}, "contents": {"source": "data:,${value}", "verification": {}}, "mode": ${decimal_mode}}}]
-EOF
-)
-    units_template=$(cat <<'EOF'
-[{"op": "add", "path": "/${path}/-", "value": {"contents": "${value}", "enabled": true, "name": "${simple_name}"}}]
-EOF
-)
-
-    kind="$1"
-    search_path="machineconfig/${kind}"
-
-    ifs="$IFS"
-    IFS=$'\n'
-    rm -fr "${PWD}/ignition_patches/generated/${kind}"
-    for file in $(find "$search_path" -type f -printf "%P\n"); do
-        path="$(dirname "$file")"
-        config_type="$(basename "$path")"
-
-        if [[ "$config_type" == "files" ]]; then
-            simple_name="$(basename "$file" | cut -f1 -d' ')"
-            dest_path="$(basename "$file" | cut -f2 -d' '| base64 -d)"
-            value="$(jq -sRr @uri "${search_path}/${file}")"
-            octal_mode=$(stat -c '%a' "${search_path}/${file}")
-            decimal_mode=$(printf "%d" "0${octal_mode}")
-            mkdir -p "${PWD}/ignition_patches/generated/${kind}"
-            path="$path" dest_path="$dest_path" value="$value" decimal_mode="$decimal_mode" envsubst <<< "$files_template" | tee "ignition_patches/generated/${kind}/${simple_name}.json"
-        elif [[ "$config_type" == "units" ]]; then
-            simple_name="$(basename "$file")"
-            if [[ "$simple_name" == *".envsubst" ]]; then
-                # The file needs to have variables substituted from env
-                content="$(envsubst < "${search_path}/${file}")"
-                simple_name="${simple_name%".envsubst"}"
-            else
-                content="$(cat "${search_path}/${file}")"
-            fi
-            value="$(sed ':a;N;$!ba;s/\n/\\n/g' <<< "$content")"
-            mkdir -p "${PWD}/ignition_patches/generated/${kind}"
-            path="$path" value="$value" simple_name="$simple_name" envsubst <<< "$units_template" | tee "ignition_patches/generated/${kind}/${simple_name}.json"
-        else
-            (>&2 echo "unknown type")
-        fi
-    done
-    IFS="$ifs"
-    set +x
-}
-
-function apply_yaml_patches() {
-    local kind
-    local target
-    local wd
-
-    kind="$1"
-    target="$2"
-    wd=$(mktemp -d)
-
-    # Convert to json so we can use jsonpatch
-    yq '.' "$target" > "${wd}/${kind}.json"
-    apply_ignition_patches "$kind" "${wd}/${kind}.json"
-
-    # Back to yaml
-    yq -y '.' < "${wd}/${kind}.json" | sudo tee "$target" | sed -e 's/.*auth.*/***PULL_SECRET***/g'
-}
-
-function create_ignition_configs() {
-    local assets_dir
-
-    assets_dir="$1"
-
-    $GOPATH/src/github.com/openshift-metalkube/kni-installer/bin/kni-install --dir "${assets_dir}" --log-level=debug create ignition-configs
+      for path in $(yq -r '.spec.config.storage.files[].path' assets/templates/${file} | cut -c 2-); do 
+          assets/yaml_patch.py "assets/generated/${file}" "/${path}" "$(cat assets/files/${path} | base64 -w0)"
+      done
+  done
 }
 
 function create_cluster() {
     local assets_dir
 
     assets_dir="$1"
+    cp ${assets_dir}/install-config.yaml{,.tmp}
 
-    $GOPATH/src/github.com/openshift-metalkube/kni-installer/bin/kni-install --dir "${assets_dir}" --log-level=debug create cluster
+    $GOPATH/src/github.com/openshift-metalkube/kni-installer/bin/kni-install --dir "${assets_dir}" --log-level=debug create ignition-configs
+    cp ${assets_dir}/master.ign{,.tmp}
+
+    cp ${assets_dir}/install-config.yaml{.tmp,}
+    $GOPATH/src/github.com/openshift-metalkube/kni-installer/bin/kni-install --dir "${assets_dir}" --log-level=debug create manifests
+
+    generate_assets
+    cp -rf assets/generated/*.yaml ${assets_dir}/openshift
+
+    cp ${assets_dir}/install-config.yaml{.tmp,}
+    $GOPATH/src/github.com/openshift-metalkube/kni-installer/bin/kni-install --dir "${assets_dir}" create cluster
+    cp ${assets_dir}/master.ign{.tmp,}
+
 }
 
 function net_iface_dhcp_ip() {
@@ -236,10 +108,6 @@ function network_ip() {
         return $rc
     fi
     echo "$ip"
-}
-
-function urldecode() {
-  echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x\1/g;')"
 }
 
 function master_node_val() {
