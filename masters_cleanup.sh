ba53484158f65a5b3a526afdafb08d3c2454e55b
Mark McLoughlin <markmc@redhat.com>
2019-03-11 21:03:09 +0000

    Use 'terraform destroy' to cleanup masters
    Now that terraform-provider-ironic supports delete.

--- masters_cleanup.sh
+++ masters_cleanup.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/bash
 
 set -eux
-source utils.sh
-source common.sh
-source ocp_install_env.sh
 
-export OS_TOKEN=fake-token
-export OS_URL=http://localhost:6385/
-
-# FIXME: this should just be "terraform delete"
-nodes=$(openstack baremetal node list)
-for node in $(jq -r .nodes[].name ${MASTER_NODES_FILE}); do
-  if [[ $nodes =~ $node ]]; then
-    openstack baremetal node undeploy $node --wait || true
-    openstack baremetal node delete $node
-  fi
-done
-
+pushd ocp/tf-master
+terraform init  # in case plugin has changed
+terraform destroy --auto-approve
+popd
 rm -rf ocp/tf-master
