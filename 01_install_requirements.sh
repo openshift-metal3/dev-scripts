dec0d9e3eba75f709813cd8231538c8e4053922b
Eduardo Minguez Perez <e.minguez@gmail.com>
2019-03-12 12:36:39 +0100

    Reordered 01 and added some comments

--- 01_install_requirements.sh
+++ 01_install_requirements.sh
@@ -6,29 +6,34 @@ source common.sh
 # FIXME ocp-doit required this so leave permissive for now
 sudo setenforce permissive
 sudo sed -i "s/=enforcing/=permissive/g" /etc/selinux/config
+
+# Update to latest packages first
 sudo yum -y update
 
+# Install EPEL required by some packages
 sudo yum -y install epel-release --enablerepo=extras
-sudo yum -y install curl vim-enhanced wget python-pip patch psmisc figlet golang dnsmasq NetworkManager crudini
 
-sudo pip install \
-    json-patch \
-    lolcat \
-    yq
-
-# for tripleo-repos install
-sudo yum -y install python-setuptools python-requests
-
-oc_version=4.0.22
-oc_tools_dir=$HOME/oc-${oc_version}
-oc_tools_local_file=openshift-client-${oc_version}.tar.gz
-if [ ! -f ${oc_tools_dir}/${oc_tools_local_file} ]; then
-  mkdir -p ${oc_tools_dir}
-  cd ${oc_tools_dir}
-  wget https://mirror.openshift.com/pub/openshift-v3/clients/${oc_version}/linux/oc.tar.gz -O ${oc_tools_local_file}
-  tar xvzf ${oc_tools_local_file}
-  sudo cp oc /usr/local/bin/
+# Work around a conflict with a newer zeromq from epel
+if ! grep -q zeromq /etc/yum.repos.d/epel.repo; then
+  sudo sed -i '/enabled=1/a exclude=zeromq*' /etc/yum.repos.d/epel.repo
 fi
+
+# Install required packages
+# python-{requests,setuptools} required for tripleo-repos install
+sudo yum -y install \
+  crudini \
+  curl \
+  dnsmasq \
+  figlet \
+  golang \
+  NetworkManager \
+  patch \
+  psmisc \
+  python-pip \
+  python-requests \
+  python-setuptools \
+  vim-enhanced \
+  wget
 
 # We're reusing some tripleo pieces for this setup so clone them here
 cd
@@ -42,27 +47,60 @@ popd
 # Needed to get a recent python-virtualbmc package
 sudo tripleo-repos current-tripleo
 
-# Work around a conflict with a newer zeromq from epel
-if ! grep -q zeromq /etc/yum.repos.d/epel.repo; then
-  sudo sed -i '/enabled=1/a exclude=zeromq*' /etc/yum.repos.d/epel.repo
-fi
+# There are some packages which are newer in the tripleo repos
 sudo yum -y update
 
-# make sure additional requirments are installed
-sudo yum install -y bind-utils ansible python-netaddr python-virtualbmc libvirt libvirt-devel libvirt-daemon-kvm qemu-kvm virt-install jq \
-		    python-ironicclient python-ironic-inspector-client python-openstackclient podman libguestfs-tools python-lxml
+# Setup yarn and nodejs repositories
+sudo curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 
+# make sure additional requirments are installed
+sudo yum -y install \
+  ansible \
+  bind-utils \
+  jq \
+  libguestfs-tools \
+  libvirt \
+  libvirt-devel \
+  libvirt-daemon-kvm \
+  nodejs \
+  podman \
+  python-ironicclient \
+  python-ironic-inspector-client \
+  python-lxml \
+  python-netaddr \
+  python-openstackclient \
+  python-virtualbmc \
+  qemu-kvm \
+  virt-install \
+  yarn
+
+# Install python packages not included as rpms
+sudo pip install \
+  json-patch \
+  lolcat \
+  yq
+
+# Install oc client
+oc_version=4.0.22
+oc_tools_dir=$HOME/oc-${oc_version}
+oc_tools_local_file=openshift-client-${oc_version}.tar.gz
+if [ ! -f ${oc_tools_dir}/${oc_tools_local_file} ]; then
+  mkdir -p ${oc_tools_dir}
+  cd ${oc_tools_dir}
+  wget https://mirror.openshift.com/pub/openshift-v3/clients/${oc_version}/linux/oc.tar.gz -O ${oc_tools_local_file}
+  tar xvzf ${oc_tools_local_file}
+  sudo cp oc /usr/local/bin/
+fi
+
+# Generate user ssh key
 if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
     ssh-keygen -f ~/.ssh/id_rsa -P ""
 fi
 
-# root needs a private key to talk to libvirt, see configure-vbmc.yml
+# root needs a private key to talk to libvirt
+# See tripleo-quickstart-config/roles/virtbmc/tasks/configure-vbmc.yml
 if sudo [ ! -f /root/.ssh/id_rsa_virt_power ]; then
-    sudo ssh-keygen -f /root/.ssh/id_rsa_virt_power -P ""
-    sudo cat /root/.ssh/id_rsa_virt_power.pub | sudo tee -a /root/.ssh/authorized_keys
+  sudo ssh-keygen -f /root/.ssh/id_rsa_virt_power -P ""
+  sudo cat /root/.ssh/id_rsa_virt_power.pub | sudo tee -a /root/.ssh/authorized_keys
 fi
-
-# Install yarn and nodejs
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
-sudo yum install yarn nodejs -y
