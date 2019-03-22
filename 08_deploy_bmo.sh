cfcff17ff0783237513f445f3d9824cae5a57fd2
Doug Hellmann <dhellmann@redhat.com>
2019-03-22 14:13:36 -0400

    be more resilient when changing the namespace for the bmo
    Change the sed pattern so we do not rely on what the current namespace
    is, only on the fact that we are changing the namespace line. This
    allows us to change the name we use when deploying on stock
    kubernetes.

--- 08_deploy_bmo.sh
+++ 08_deploy_bmo.sh
@@ -8,7 +8,7 @@ export BMOPATH="$GOPATH/src/github.com/metalkube/baremetal-operator"
 
 # Make a local copy of the baremetal-operator code to make changes
 cp -r $BMOPATH/deploy ocp/.
-sed -i 's/bmo-project/openshift-machine-api/g' ocp/deploy/role_binding.yaml
+sed -i 's/namespace: .*/namespace: openshift-machine-api/g' ocp/deploy/role_binding.yaml
 
 # Start deploying on the new cluster
 oc --config ocp/auth/kubeconfig apply -f ocp/deploy/service_account.yaml --namespace=openshift-machine-api
