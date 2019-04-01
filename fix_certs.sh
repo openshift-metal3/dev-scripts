74b50c32ee71287d99a0b44f87d136aee252a31b
karmab <karimboumedhel@gmail.com>
2019-04-01 12:20:08 +0200

    allow to run fixcerts from cron

--- fix_certs.sh
+++ fix_certs.sh
@@ -2,4 +2,5 @@
 
 # https://github.com/openshift-metalkube/dev-scripts/issues/141#issuecomment-474331659
 
-oc --config ocp/auth/kubeconfig get csr -o name | xargs -n 1 oc --config ocp/auth/kubeconfig adm certificate approve
+export KUBECONFIG=$(dirname $0)/ocp/auth/kubeconfig
+oc get csr -o name | xargs -n 1 oc adm certificate approve
