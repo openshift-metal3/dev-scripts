d51f65b0c5a9adef45c19a58b7a96d6bb1576045
Umanga Chapagain <chapagainumanga@gmail.com>
2019-04-17 22:49:02 +0530

    Bumped ceph version to v14(nautilus) and some fixes
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- 10_deploy_rook.sh
+++ 10_deploy_rook.sh
@@ -7,6 +7,7 @@ source common.sh
 figlet "Deploying rook" | lolcat
 eval "$(go env)"
 
+CEPH_VERSION=v14
 export MIXINPATH="$GOPATH/src/github.com/ceph/ceph-mixins"
 export ROOKPATH="$GOPATH/src/github.com/rook/rook"
 cd $ROOKPATH/cluster/examples/kubernetes/ceph
@@ -17,9 +18,7 @@ oc create -f common-modified.yaml
 oc label namespace openshift-storage  "openshift.io/cluster-monitoring=true"
 oc policy add-role-to-user view system:serviceaccount:openshift-monitoring:prometheus-k8s -n openshift-storage
 
-sed '/FLEXVOLUME_DIR_PATH/!b;n;c\          value: "\/etc/kubernetes\/kubelet-plugins\/volume\/exec"' operator-openshift.yaml > operator-openshift-modified.yaml
-sed -i 's/# - name: FLEXVOLUME_DIR_PATH/- name: FLEXVOLUME_DIR_PATH/' operator-openshift-modified.yaml
-sed -i 's/namespace: rook-ceph/namespace: openshift-storage/' operator-openshift-modified.yaml
+sed 's/namespace: rook-ceph/namespace: openshift-storage/' operator-openshift.yaml > operator-openshift-modified.yaml
 sed -i 's/:rook-ceph:/:openshift-storage:/' operator-openshift-modified.yaml
 oc create -f operator-openshift-modified.yaml
 sleep 120
@@ -31,6 +30,8 @@ oc wait --for condition=ready  pod -l app=rook-discover -n openshift-storage --t
 sed "s/useAllDevices: .*/useAllDevices: true/" cluster.yaml > cluster-modified.yaml
 sed -i 's/# port: 8443/port: 8444/' cluster-modified.yaml
 sed -i 's/namespace: rook-ceph/namespace: openshift-storage/' cluster-modified.yaml
+sed -i 's/allowUnsupported: false/allowUnsupported: true/' cluster-modified.yaml
+sed -i 's/image: ceph\/ceph.*/image: ceph\/ceph:$CEPH_VERSION/' cluster-modified.yaml
 oc create -f cluster-modified.yaml
 sleep 120
 
@@ -85,6 +86,7 @@ EOF
 cd $ROOKPATH/cluster/examples/kubernetes/ceph/monitoring
 sed 's/namespace: rook-ceph/namespace: openshift-storage/' service-monitor.yaml > service-monitor-modified.yaml
 sed -i 's/- rook-ceph/- openshift-storage/' service-monitor-modified.yaml
+sed -i 's/rook_cluster: rook-ceph/rook_cluster: openshift-storage/' service-monitor-modified.yaml
 oc create -f service-monitor-modified.yaml
 
 cd $MIXINPATH/manifests
