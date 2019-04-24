ca6d066459c9c9febd0d30dfb9b3f7043867aa04
Russell Bryant <rbryant@redhat.com>
2019-04-24 08:49:11 -0400

    add-machine-ips: Drop oc proxy.
    oc proxy is now run inside link-machine-and-node.sh instead.

--- add-machine-ips.sh
+++ add-machine-ips.sh
@@ -7,12 +7,6 @@ source utils.sh
 source common.sh
 source ocp_install_env.sh
 
-# BEGIN Hack #260
-# Hack workaround for openshift-metalkube/dev-scripts#260 until it's done automatically
-# Also see https://github.com/metalkube/cluster-api-provider-baremetal/issues/49
-oc --config ocp/auth/kubeconfig proxy &
-proxy_pid=$!
-
 for node in $(oc --config ocp/auth/kubeconfig get nodes -o template --template='{{range .items}}{{.metadata.uid}}:{{.metadata.name}}{{"\n"}}{{end}}'); do
     node_name=$(echo $node | cut -f2 -d':')
     machine_name=$CLUSTER_NAME-$node_name
@@ -21,5 +15,3 @@ for node in $(oc --config ocp/auth/kubeconfig get nodes -o template --template='
     fi
     $SCRIPTDIR/link-machine-and-node.sh "$machine_name" "$node"
 done
-
-kill $proxy_pid
