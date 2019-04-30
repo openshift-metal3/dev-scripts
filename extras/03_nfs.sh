1afd891ec9b586251f47fec66f21b486b247d859
karmab <karimboumedhel@gmail.com>
2019-04-30 15:13:17 +0200

    use iptables for nfs firewall rules

--- extras/03_nfs.sh
+++ extras/03_nfs.sh
@@ -11,6 +11,16 @@ sudo chcon -t svirt_sandbox_file_t /windows
 sudo chmod 777 /windows
 sudo exportfs -r
 sudo systemctl start nfs ; sudo systemctl enable nfs-server
+#Allow access to nfs
+for port in 111 2049 20048 ; do
+    if ! sudo iptables -C INPUT -i baremetal -p udp --dport $port -j ACCEPT 2>/dev/null ; then
+        sudo iptables -I INPUT -i baremetal -p udp --dport $port -j ACCEPT
+    fi
+    if ! sudo iptables -C INPUT -i baremetal -p tcp --dport $port -j ACCEPT 2>/dev/null ; then
+        sudo iptables -I INPUT -i baremetal -p tcp --dport $port -j ACCEPT
+    fi
+done
+
 sudo firewall-cmd --permanent --add-service=nfs
 sudo firewall-cmd --permanent --add-service=mountd
 sudo firewall-cmd --permanent --add-service=rpc-bind
