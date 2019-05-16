3434feaf1b2cb4b76c52b75c885d3f096889fa54
Itzik Brown <itzikb@redhat.com>
2019-05-16 17:10:40 +0300

    Add a timeout when getting ironic images

--- get_images.sh
+++ get_images.sh
@@ -6,11 +6,11 @@ source common.sh
 mkdir -p "$IRONIC_DATA_DIR/html/images"
 pushd "$IRONIC_DATA_DIR/html/images"
 if [ ! -f "${RHCOS_IMAGE_FILENAME_OPENSTACK}" ]; then
-    curl --insecure --compressed -L -o "${RHCOS_IMAGE_FILENAME_OPENSTACK}" "${RHCOS_IMAGE_URL}/${RHCOS_IMAGE_FILENAME_OPENSTACK}"
+    curl --insecure --compressed --connect-timeout 120 -L -o "${RHCOS_IMAGE_FILENAME_OPENSTACK}" "${RHCOS_IMAGE_URL}/${RHCOS_IMAGE_FILENAME_OPENSTACK}"
 fi
 
 if [ ! -f ironic-python-agent.initramfs ]; then
-    curl --insecure --compressed -L https://images.rdoproject.org/master/rdo_trunk/current-tripleo-rdo/ironic-python-agent.tar | tar -xf -
+    curl --insecure --compressed --connect-timeout 120 -L https://images.rdoproject.org/master/rdo_trunk/current-tripleo-rdo/ironic-python-agent.tar | tar -xf -
 fi
 
 popd
