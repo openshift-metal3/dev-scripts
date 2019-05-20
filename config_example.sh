492ca77a65b300a5715822a7ea29da12a631b359
Russell Bryant <rbryant@redhat.com>
2019-05-20 08:44:48 -0400

    Allow setting SSH_PUB_KEY in config.

--- config_example.sh
+++ config_example.sh
@@ -9,3 +9,8 @@ set -x
 # Uncomment to build a copy of ironic or inspector locally
 #export IRONIC_INSPECTOR_IMAGE=https://github.com/metal3-io/ironic-inspector
 #export IRONIC_IMAGE=https://github.com/metal3-io/ironic
+
+# SSH key used to ssh into deployed hosts.  This must be the contents of the
+# variable, not the filename. The contents of ~/.ssh/id_rsa.pub are used by
+# default.
+#export SSH_PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
