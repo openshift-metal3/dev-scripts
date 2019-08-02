a514cfc08a6e7356c220a1a2cb9d77583caf33d1
Stephen Benjamin <stephen@redhat.com>
2019-08-02 10:52:14 -0400

    Comment out 04

--- 04_setup_ironic.sh
+++ 04_setup_ironic.sh
@@ -7,8 +7,7 @@ source common.sh
 
 # Either pull or build the ironic images
 # To build the IRONIC image set
-# IRONIC_IMAGE=https://github.com/metalkube/metalkube-ironic
-for IMAGE_VAR in IRONIC_IMAGE IRONIC_INSPECTOR_IMAGE IPA_DOWNLOADER_IMAGE COREOS_DOWNLOADER_IMAGE ; do
+for IMAGE_VAR in IPA_DOWNLOADER_IMAGE ; do
     IMAGE=${!IMAGE_VAR}
     # Is it a git repo?
     if [[ "$IMAGE" =~ "://" ]] ; then
@@ -30,12 +29,12 @@ for name in ironic ironic-api ironic-conductor ironic-inspector dnsmasq httpd ma
 done
 
 # Remove existing pod
-if  sudo podman pod exists ironic-pod ; then 
+if  sudo podman pod exists ironic-pod ; then
     sudo podman pod rm ironic-pod -f
 fi
 
 # Create pod
-sudo podman pod create -n ironic-pod 
+sudo podman pod create -n ironic-pod
 
 # We start only the httpd and *downloader containers so that we can provide
 # cached images to the bootstrap VM
@@ -45,8 +44,7 @@ sudo podman run -d --net host --privileged --name httpd --pod ironic-pod \
 sudo podman run -d --net host --privileged --name ipa-downloader --pod ironic-pod \
      -v $IRONIC_DATA_DIR:/shared ${IPA_DOWNLOADER_IMAGE} /usr/local/bin/get-resource.sh
 
-sudo podman run -d --net host --privileged --name coreos-downloader --pod ironic-pod \
-     -v $IRONIC_DATA_DIR:/shared ${COREOS_DOWNLOADER_IMAGE} /usr/local/bin/get-resource.sh $RHCOS_IMAGE_URL
-
 # Wait for images to be downloaded/ready
-while ! curl --fail http://localhost:80/images/rhcos-ootpa-latest.qcow2.md5sum ; do sleep 1 ; done
+while ! curl --fail --head http://localhost/images/ironic-python-agent.initramfs ; do sleep 1; done
+while ! curl --fail --head http://localhost/images/ironic-python-agent.tar.headers ; do sleep 1; done
+while ! curl --fail --head http://localhost/images/ironic-python-agent.kernel ; do sleep 1; done
