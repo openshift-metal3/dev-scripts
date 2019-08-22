04c204c4a2258014b7f81b51520ec8d9967420ba
Doug Hellmann <dhellmann@redhat.com>
2019-08-22 11:55:29 -0400

    add more tool scripts for showing bootstrap logs
    Add a script that connects to the bootstrap node to show logs for a
    service passed as argument, then update the existing scripts to use
    that.
    
    Add a new script to look at the ironic logs.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- bootkube_log.sh
+++ bootkube_log.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-BOOTSTRAP_VM_IP=$(sudo virsh net-dhcp-leases baremetal | grep -v master | grep ipv4 | tail -n1 | sed -e 's/.*\(192.*\)\/.*/\1/')
-echo "Attempting to follow bootkube on ${BOOTSTRAP_VM_IP} ..."
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no core@${BOOTSTRAP_VM_IP} journalctl -b -f -u bootkube.service
+$(dirname $0)/show_bootstrap_log.sh bootkube.service
