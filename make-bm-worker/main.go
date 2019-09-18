b1daee07c87fd7b3695af82811e6cc9875077055
Steven Hardy <shardy@redhat.com>
2019-09-18 10:52:00 +0100

    Simplify make-bm-worker/main.go
    This removes the interfaces for Image, Machine and ExternallyProvisioned
    since all those things are now correctly set via the actuator.

--- make-bm-worker/main.go
+++ make-bm-worker/main.go
@@ -9,11 +9,6 @@ import (
 	"text/template"
 )
 
-const (
-	instanceImageSource      = "http://172.22.0.1/images/rhcos-ootpa-latest.qcow2"
-	instanceImageChecksumURL = instanceImageSource + ".md5sum"
-)
-
 var templateBody = `---
 apiVersion: v1
 kind: Secret
@@ -36,37 +31,16 @@ spec:
     credentialsName: {{ .Name }}-bmc-secret
 {{- if .MAC }}
   bootMACAddress: {{ .MAC }}
-{{ end -}}{{- if .WithImage }}
-  userData:
-    namespace: openshift-machine-api
-    name: worker-user-data
-  image:
-    url: "{{ .ImageSourceURL }}"
-    checksum: "{{ .Checksum }}"
-{{ end -}}{{ if .Machine }}
-  consumerRef:
-    name: {{ .Machine }}
-    kind: Machine
-    apiVersion: machine.openshift.io/v1beta1
-    namespace: {{ .MachineNamespace }}
-{{ end }}{{ if .ExternallyProvisioned }}
-  externallyProvisioned: true
-{{ end}}
+{{ end -}}
 `
 
 // TemplateArgs holds the arguments to pass to the template.
 type TemplateArgs struct {
-	Name                  string
-	BMCAddress            string
-	MAC                   string
-	EncodedUsername       string
-	EncodedPassword       string
-	WithImage             bool
-	Checksum              string
-	ImageSourceURL        string
-	Machine               string
-	MachineNamespace      string
-	ExternallyProvisioned bool
+	Name            string
+	BMCAddress      string
+	MAC             string
+	EncodedUsername string
+	EncodedPassword string
 }
 
 func encodeToSecret(input string) string {
@@ -78,16 +52,8 @@ func main() {
 	var password = flag.String("password", "", "password for BMC")
 	var bmcAddress = flag.String("address", "", "address URL for BMC")
 	var verbose = flag.Bool("v", false, "turn on verbose output")
-	var withImage = flag.Bool("image", false, "include the image settings to trigger deployment")
-	var machine = flag.String(
-		"machine", "", "specify name of a related, existing, Machine to link")
-	var machineNamespace = flag.String(
-		"machine-namespace", "", "specify namespace of a related, existing, Machine to link")
 	var bootMAC = flag.String(
 		"boot-mac", "", "specify boot MAC address of host")
-	var externallyProvisioned = flag.Bool("externally-provisioned", false,
-		"the host was externally provisioned (typically a master node)")
-
 	flag.Parse()
 
 	hostName := flag.Arg(0)
@@ -109,17 +75,11 @@ func main() {
 	}
 
 	args := TemplateArgs{
-		Name:                  strings.Replace(hostName, "_", "-", -1),
-		BMCAddress:            *bmcAddress,
-		MAC:                   *bootMAC,
-		EncodedUsername:       encodeToSecret(*username),
-		EncodedPassword:       encodeToSecret(*password),
-		WithImage:             *withImage,
-		Checksum:              instanceImageChecksumURL,
-		ImageSourceURL:        instanceImageSource,
-		Machine:               strings.TrimSpace(*machine),
-		MachineNamespace:      strings.TrimSpace(*machineNamespace),
-		ExternallyProvisioned: *externallyProvisioned,
+		Name:            strings.Replace(hostName, "_", "-", -1),
+		BMCAddress:      *bmcAddress,
+		MAC:             *bootMAC,
+		EncodedUsername: encodeToSecret(*username),
+		EncodedPassword: encodeToSecret(*password),
 	}
 	if *verbose {
 		fmt.Fprintf(os.Stderr, "%v", args)
