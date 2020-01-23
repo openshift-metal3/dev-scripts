f163a80f45b65276a0832053f39ecbab782f5970
Russell Bryant <rbryant@redhat.com>
2020-01-23 10:39:35 -0500

    ipv6: Remove workaround for haproxy configs on masters
    This is handled automatically as of
    4.3.0-0.nightly-2020-01-21-205041-ipv6.3

--- v6workarounds.sh
+++ v6workarounds.sh
@@ -9,16 +9,8 @@ fi
 BOOTSTRAP_IP="$1"
 DNS_VIP=$2
 
-MASTER_IPS="fd2e:6f44:5dd8:c956::14 fd2e:6f44:5dd8:c956::15 fd2e:6f44:5dd8:c956::16"
-
 fssh() {
     ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $@
 }
-
-for MASTER in ${MASTER_IPS} ; do
-    # haproxy not configured to listen on IPv6
-    fssh core@${MASTER} sudo sed -i \"s/bind :7443/bind :::7443 v4v6/\" /etc/haproxy/haproxy.cfg
-    fssh core@${MASTER} sudo sed -i \"s/bind :50936/bind :::50936 v4v6/\" /etc/haproxy/haproxy.cfg
-done
 
 fssh core@${BOOTSTRAP_IP} sudo sed -i \"1s/^/nameserver ${DNS_VIP}\\n/\" /etc/resolv.conf
