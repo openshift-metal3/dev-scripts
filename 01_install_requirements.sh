794ad2b3aff0f6116661e60286babd213ba88d01
Stephen Benjamin <stephen@redhat.com>
2020-01-28 09:09:35 -0500

    Don't install `oc` in CI and allow override of version
    The openshift CI jobs will extract `oc` from one of the containers,
    and the version isn't determinable from images built in CI anyway. This
    also lets a user override the OPENSHIFT_VERSION in case they want to
    install some specific version for some reason.

--- 01_install_requirements.sh
+++ 01_install_requirements.sh
@@ -32,20 +32,22 @@ if sudo systemctl is-active docker-distribution.service; then
   sudo systemctl disable --now docker-distribution.service
 fi
 
-# Install oc client
-oc_version=${OPENSHIFT_VERSION}
-oc_tools_dir=$HOME/oc-${oc_version}
-oc_tools_local_file=openshift-client-${oc_version}.tar.gz
-if which oc 2>&1 >/dev/null ; then
-  oc_git_version=$(oc version -o json | jq -r '.clientVersion.gitVersion')
-  oc_actual_version=$(echo "${oc_git_version}" | sed "s/.*-\([[:digit:]]\.[[:digit:]]\)\.[[:digit:]]-.*/\1/")
-fi
-if [ ! -f ${oc_tools_dir}/${oc_tools_local_file} ] || [ "$oc_actual_version" != "$oc_version" ]; then
-  mkdir -p ${oc_tools_dir}
-  cd ${oc_tools_dir}
-  wget https://mirror.openshift.com/pub/openshift-v4/clients/oc/${oc_version}/linux/oc.tar.gz -O ${oc_tools_local_file}
-  tar xvzf ${oc_tools_local_file}
-  sudo cp oc /usr/local/bin/
+# Install oc client - unless we're in openshift CI
+if [[ -z "$OPENSHIFT_CI" ]]; then
+  oc_version=${OPENSHIFT_VERSION:-$(echo $OPENSHIFT_RELEASE_IMAGE | sed "s/.*:\([[:digit:]]\.[[:digit:]]\).*/\1/")}
+  oc_tools_dir=$HOME/oc-${oc_version}
+  oc_tools_local_file=openshift-client-${oc_version}.tar.gz
+  if which oc 2>&1 >/dev/null ; then
+    oc_git_version=$(oc version -o json | jq -r '.clientVersion.gitVersion')
+    oc_actual_version=$(echo "${oc_git_version}" | sed "s/.*-\([[:digit:]]\.[[:digit:]]\)\.[[:digit:]]-.*/\1/")
+  fi
+  if [ ! -f ${oc_tools_dir}/${oc_tools_local_file} ] || [ "$oc_actual_version" != "$oc_version" ]; then
+    mkdir -p ${oc_tools_dir}
+    cd ${oc_tools_dir}
+    wget https://mirror.openshift.com/pub/openshift-v4/clients/oc/${oc_version}/linux/oc.tar.gz -O ${oc_tools_local_file}
+    tar xvzf ${oc_tools_local_file}
+    sudo cp oc /usr/local/bin/
+  fi
 fi
 
 # Install operator-sdk
