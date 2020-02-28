f3295d1433aea12373d4e0797ab35bd299253f03
Doug Hellmann <dhellmann@redhat.com>
2020-02-28 09:11:12 -0500

    be explicit about all VIPs
    Add apiVIP and ingressVIP to the install-config.yaml produced since
    those values may change based on the cluster.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- ocp_install_env.sh
+++ ocp_install_env.sh
@@ -112,6 +112,8 @@ $(network_configuration)
     provisioningBridge: ${PROVISIONING_NETWORK_NAME}
     bootstrapOSImage: http://$(wrap_if_ipv6 $MIRROR_IP)/images/${MACHINE_OS_BOOTSTRAP_IMAGE_NAME}?sha256=${MACHINE_OS_BOOTSTRAP_IMAGE_UNCOMPRESSED_SHA256}
     clusterOSImage: http://$(wrap_if_ipv6 $MIRROR_IP)/images/${MACHINE_OS_IMAGE_NAME}?sha256=${MACHINE_OS_IMAGE_SHA256}
+    apiVIP: ${API_VIP}
+    ingressVIP: ${INGRESS_VIP}
     dnsVIP: ${DNS_VIP}
     hosts:
 $(node_map_to_install_config_hosts $NUM_MASTERS 0 master)
