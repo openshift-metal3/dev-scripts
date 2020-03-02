39802468d568c4687ebc44adeb9eb9d697139daf
Doug Hellmann <dhellmann@redhat.com>
2020-03-02 14:56:16 -0500

    support a log file prefix for logs that "go together"
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- logging.sh
+++ logging.sh
@@ -1,9 +1,13 @@
 # Log output automatically
 LOGDIR="$(dirname $0)/logs"
-if [ ! -d "$LOGDIR" ]; then
-    mkdir -p "$LOGDIR"
+if [[ -z "${LOGPREFIX:-}" ]]; then
+    LOGFILE="$LOGDIR/$(basename $0 .sh)-$(date +%F-%H%M%S).log"
+else
+    LOGFILE="$LOGDIR/${LOGPREFIX}$(basename $0 .sh)-$(date +%F-%H%M%S).log"
 fi
-LOGFILE="$LOGDIR/$(basename $0 .sh)-$(date +%F-%H%M%S).log"
+if [ ! -d "$(dirname $LOGFILE)" ]; then
+    mkdir -p "$(dirname $LOGFILE)"
+fi
 echo "Logging to $LOGFILE"
 # Set fd 1 and 2 to write to the log file
 exec 1> >( tee "${LOGFILE}" ) 2>&1
