2e79b5f27dd3c924689da875ddc96d1dfe016bab
Stephen Benjamin <stephen@redhat.com>
2020-03-23 17:05:32 -0400

    Calculcate mirrored pullspec correctly

--- must_gather.sh
+++ must_gather.sh
@@ -10,8 +10,14 @@ if [ ! -d "$MUST_GATHER_PATH" ]; then
     mkdir -p "$MUST_GATHER_PATH"
 fi
 
-if [ ! -z "${MIRROR_IMAGES}" ]; then
-  MUST_GATHER_IMAGE="--image=${LOCAL_REGISTRY_DNS_NAME}:${LOCAL_REGISTRY_PORT}/localimages/local-release-image:${OPENSHIFT_RELEASE_TAG}-must-gather"
+# must-gather doesn't correctly work in disconnected environment, so we
+# have to calculcate the pullspec for the image and pass it to oc
+if [ -n "${MIRROR_IMAGES}" ]; then
+  pullsecret_file=$(mktemp "pullsecret--XXXXXXXXXX")
+  echo "${PULL_SECRET}" > "${pullsecret_file}"
+
+  OPENSHIFT_RELEASE_VERSION=$(oc adm release info --registry-config="$pullsecret_file" "$OPENSHIFT_RELEASE_IMAGE" | jq -r ".config.config.Labels.\"io.openshift.release\"")
+  MUST_GATHER_IMAGE="--image=${LOCAL_REGISTRY_DNS_NAME}:${LOCAL_REGISTRY_PORT}/localimages/local-release-image:${OPENSHIFT_RELEASE_VERSION}-must-gather"
 else
   MUST_GATHER_IMAGE=""
 fi
