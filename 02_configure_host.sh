c800c819fcfc30e458b8098aedee183d41433350
Steven Hardy <shardy@redhat.com>
2020-07-29 12:19:57 +0100

    Fix firewall issues with NUM_EXTRA_WORKERS
    There are two problems - the base port is miscalculated so we
    have a discontinuous port range, and we don't consider the extra
    workers in the calculation of VBMC_MAX_PORT.
    
    This means BMH registration fails when adding the extra workers
    to the cluster.

--- 02_configure_host.sh
+++ 02_configure_host.sh
@@ -87,7 +87,7 @@ if [ ${NUM_EXTRA_WORKERS} -ne 0 ]; then
         -e "manage_baremetal=$MANAGE_BR_BRIDGE" \
         -e "provisioning_url_host=$PROVISIONING_URL_HOST" \
         -e "nodes_file=$EXTRA_NODES_FILE" \
-        -e "virtualbmc_base_port=$(( $VBMC_BASE_PORT + $NUM_MASTERS + $NUM_WORKERS + 1 ))" \
+        -e "virtualbmc_base_port=$(($VBMC_BASE_PORT + $NUM_MASTERS + $NUM_WORKERS))" \
         -e "master_hostname_format=$MASTER_HOSTNAME_FORMAT" \
         -e "worker_hostname_format=extra-$WORKER_HOSTNAME_FORMAT" \
         -i ${VM_SETUP_PATH}/inventory.ini \
