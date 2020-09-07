aa1a462f2d5e6f2ccae2338c5252638a245db2b4
Andrea Fasano <afasano@redhat.com>
2020-09-07 19:35:43 +0200

    New script to test local cluster baremetal operator code

---
+++ metal3-dev/local-cbo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+LOGDIR=${SCRIPTDIR}/logs
+source $SCRIPTDIR/logging.sh
+
+source $SCRIPTDIR/common.sh
+source $SCRIPTDIR/utils.sh
+
+cbo_path=$GOPATH/src/github.com/openshift/cluster-baremetal-operator
+if [ ! -d $cbo_path ]; then
+    echo "Did not find $cbo_path" 1>&2
+    exit 1
+fi
+
+# Run the operator
+cd $cbo_path
+
+export RUN_NAMESPACE=openshift-machine-api
+make run
+
