4896abd135e9f0501e4a3a3a6ace3f8a247afe1d
Zane Bitter <zbitter@redhat.com>
2020-09-14 23:06:13 -0400

    Avoid logging auth tokens/pull secrets

--- common.sh
+++ common.sh
@@ -273,9 +273,11 @@ export PULL_SECRET_FILE=${PULL_SECRET_FILE:-$WORKING_DIR/pull_secret.json}
 
 # Ensure a few variables are set, even if empty, to avoid undefined
 # variable errors in the next 2 checks.
+set +x
 export PULL_SECRET=${PULL_SECRET:-}
-export PERSONAL_PULL_SECRET=${PERSONAL_PULL_SECRET:-$SCRIPTDIR/pull_secret.json}
 export CI_TOKEN=${CI_TOKEN:-}
+set -x
+export PERSONAL_PULL_SECRET=${PERSONAL_PULL_SECRET:-$SCRIPTDIR/pull_secret.json}
 
 # avoid "-z $PULL_SECRET" to ensure the secret is not logged
 if [ ! -s ${PERSONAL_PULL_SECRET} -a ${#PULL_SECRET} = 0 ]; then
