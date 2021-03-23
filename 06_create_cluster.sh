b256b9cc23c7683121941b4312bcd6bef3198872
Arda Guclu <aguclu@redhat.com>
2021-03-23 02:29:01 -0400

    Configure cluster samples operator and tools image registry

--- 06_create_cluster.sh
+++ 06_create_cluster.sh
@@ -26,4 +26,14 @@ done
 oc patch configs.imageregistry.operator.openshift.io \
     cluster --type merge --patch '{"spec":{"storage":{"emptyDir":{}},"managementState":"Managed"}}'
 
+if [[ ! -z "${ENABLE_LOCAL_REGISTRY}" ]]; then        
+    # Configure tools image registry and cluster samples operator 
+    # when local image stream is enabled. These are basically to run CI tests
+    # depend on tools image.
+    add_local_certificate_as_trusted
+    
+    oc patch configs.samples cluster \
+      --type merge --patch "{\"spec\":{\"samplesRegistry\":\"${LOCAL_REGISTRY_DNS_NAME}\"}}"
+fi
+
 echo "Cluster up, you can interact with it via oc --config ${KUBECONFIG} <command>"
