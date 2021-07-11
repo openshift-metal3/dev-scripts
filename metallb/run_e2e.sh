c4bfc36c31b71cd4518f9c1feea0a3386a554f3d
Tomofumi Hayashi <tohayash@redhat.com>
2021-07-11 23:35:50 +0900

    Fix run_e2e.sh script (#1275)

--- metallb/run_e2e.sh
+++ metallb/run_e2e.sh
@@ -7,8 +7,8 @@ if [ ! -d ./metallb ]; then
 	git clone $METALLB_REPO
 fi
 cd metallb
-git checkout dev/support-e2e-non-kind-env
 
 pip3 install --user -r ./dev-env/requirements.txt
+export PATH=${PATH}:${HOME}/.local/bin
 inv e2etest --kubeconfig=$(readlink -f ../../ocp/ostest/auth/kubeconfig) \
 	--service-pod-port=8080 --system-namespaces="metallb-system" --skip-docker
