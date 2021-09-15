93a1a641b678f9585e49d013a6f884d67725c015
Logan Blyth <lblyth@redhat.com>
2021-09-15 20:41:29 +0000

    trying to fix ipv6 bug

--- 02_configure_host.sh
+++ 02_configure_host.sh
@@ -132,7 +132,7 @@ if [ "$MANAGE_PRO_BRIDGE" == "y" ]; then
         if [[ "$(ipversion $PROVISIONING_HOST_IP)" == "6" ]]; then
 	    sudo nmcli con modify ${PROVISIONING_NETWORK_NAME} ipv6.address $PROVISIONING_HOST_IP/64
 	    # a /0 route will create a static route for that address, hopefully not messing with the rest of the system
-	    sudo nmcli con modify ${PROVISIONING_NETWORK_NAME} ipv6.routes $PROVISIONING_HOST_IP/0
+	    sudo nmcli con modify ${PROVISIONING_NETWORK_NAME} ipv6.routes $PROVISIONING_NETWORK
 	    # manual should be specified after the address
 	    sudo nmcli con modify ${PROVISIONING_NETWORK_NAME} ipv6.method manual
         else
