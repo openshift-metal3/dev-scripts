918da5cba37ec38b47e67dbe08d53297e8dcb7ea
Sabina Aledort <saledort@redhat.com>
2021-11-22 23:58:31 +0200

    Adjust MetalLB E2E tests run to the new tests configuration in MetalLB repo (#1305)
    MetalLB E2E tests are now creating and deleting MetalLB configuration
    when running.

--- metallb/run_e2e.sh
+++ metallb/run_e2e.sh
@@ -8,7 +8,24 @@ if [ ! -d ./metallb ]; then
 fi
 cd metallb
 
+# add firewalld rules
+sudo firewall-cmd --zone=libvirt --permanent --add-port=179/tcp
+sudo firewall-cmd --zone=libvirt --add-port=179/tcp
+sudo firewall-cmd --zone=libvirt --permanent --add-port=180/tcp
+sudo firewall-cmd --zone=libvirt --add-port=180/tcp
+
+SKIP="none"
+if [ "${IP_STACK}" = "v4" ]; then SKIP="${SKIP}\|IPV6"; fi
+if [ "${IP_STACK}" = "dual" ]; then SKIP="${SKIP}\|IPV6"; fi
+if [ "${IP_STACK}" = "v6" ]; then SKIP="${SKIP}\|IPV4\|BGP"; fi
+echo "Skipping ${SKIP}"
+
 pip3 install --user -r ./dev-env/requirements.txt
 export PATH=${PATH}:${HOME}/.local/bin
+export CONTAINER_RUNTIME=podman
+export RUN_FRR_CONTAINER_ON_HOST_NETWORK=true
+export PROVISIONING_HOST_EXTERNAL_IPV4=${PROVISIONING_HOST_EXTERNAL_IP}
 inv e2etest --kubeconfig=$(readlink -f ../../ocp/ostest/auth/kubeconfig) \
-	--service-pod-port=8080 --system-namespaces="metallb-system" --skip-docker
+	--service-pod-port=8080 --system-namespaces="metallb-system" --skip-docker \
+	--ipv4-service-range=192.168.10.0/24 --ipv6-service-range=fc00:f853:0ccd:e799::/124 \
+	--skip=${SKIP}
