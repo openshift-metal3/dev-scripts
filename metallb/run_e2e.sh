892970f687f7420e63a76808c47a9672b0467a97
Sabina Aledort <saledort@redhat.com>
2021-12-05 10:13:17 +0200

    MetalLB E2E tests: Fix skip flag
    This is done to fix the following error:
    
    skip="L2' 'metrics"\|IPV6\|DUALSTACK'
    No idea what 'metrics"\\|IPV6\\|DUALSTACK' is!
    make: Leaving directory '/root/dev-scripts/metallb'
    make: *** [Makefile:19: run_e2e] Error 1
    
    As there are spaces in the skip string variable
    we need to put quotes on it.

--- metallb/run_e2e.sh
+++ metallb/run_e2e.sh
@@ -42,4 +42,4 @@ export RUN_FRR_CONTAINER_ON_HOST_NETWORK=true
 inv e2etest --kubeconfig=$(readlink -f ../../ocp/ostest/auth/kubeconfig) \
 	--service-pod-port=8080 --system-namespaces="metallb-system" --skip-docker \
 	--ipv4-service-range=192.168.10.0/24 --ipv6-service-range=fc00:f853:0ccd:e799::/124 \
-	--skip=${SKIP}
+	--skip="${SKIP}"
