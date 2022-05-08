c6ac43961a5c11c5a169e7403c193f53fa9e1682
Sabina Aledort <saledort@redhat.com>
2022-05-08 14:37:17 +0300

    MetalLB: Remove MetalLB deployment scripts
    The MetalLB deployment scripts for OCP CI are now part
    of the openshift/metallb repo:
    https://github.com/openshift/metallb/tree/main/openshift-ci

--- metallb/metallb_common.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-METALLB_IMAGE_BASE=${METALLB_IMAGE_BASE:-"quay.io/metallb"}
-METALLB_IMAGE_TAG=${METALLB_IMAGE_TAG:-"main"}
-
-metallb_dir="$(dirname $(readlink -f $0))"
-source ${metallb_dir}/../common.sh
-source ${metallb_dir}/../network.sh
