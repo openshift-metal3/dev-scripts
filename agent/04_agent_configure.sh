6ae1790f03ca5c6706ecabb9c4734aec30d2ab40
Andrea Fasano <60063538+andfasano@users.noreply.github.com>
2022-09-13 16:08:46 +0200

    agent: fixes for using 4.12 branch (#1450)
    * agent: use binary release version
    
    * agent: use master branch when building installer from sources

--- agent/04_agent_configure.sh
+++ agent/04_agent_configure.sh
@@ -138,6 +138,11 @@ EOF
     local releaseImage=${OPENSHIFT_RELEASE_IMAGE}
     if [ ! -z "${MIRROR_IMAGES}" ]; then
         releaseImage="${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}"
+    # If not installing from src, let's use the current version from the binary
+    elif [ -z "$KNI_INSTALL_FROM_GIT" ]; then
+      local openshift_install="$(realpath "${OCP_DIR}/openshift-install")"
+      releaseImage=$("${openshift_install}" --dir="${OCP_DIR}" version | grep "release image" | cut -d " " -f 3)
+      echo "Setting release image to ${releaseImage}"
     fi
 
     cat > "${MANIFESTS_PATH}/cluster-image-set.yaml" << EOF
