f83c02bd7533a79c9e4a18d14daf7b2bdc2da657
Pawan Pinjarkar <ppinjark@redhat.com>
2022-12-05 15:25:43 -0600

    Use node zero IP in a better way

--- agent/gather.sh
+++ agent/gather.sh
@@ -5,21 +5,15 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 source $SCRIPTDIR/agent/04_agent_configure.sh
 
-get_static_ips_and_macs
-
-if [[ "$IP_STACK" = "v4" ]]; then
-    ip=${AGENT_NODES_IPS[0]}
-  else
-    ip=${AGENT_NODES_IPSV6[0]}
-  fi
+ip=$(head -n 1 ${OCP_DIR}/node0-ip)
 
 ssh -o 'ConnectTimeout=30' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' core@${ip} << EOF
 # check is agent-gather command is present
 if ! command -v /usr/local/bin/agent-gather &> /dev/null
 then
-    echo "/usr/local/bin/agent-gather could not be found."
-    exit
+    echo "Skipping gathering agent logs, agent-gather script not present."
+    exit 0
 fi
 
-sudo /usr/local/bin/agent-gather -O | tar -xJvf -
+sudo /usr/local/bin/agent-gather
 EOF
