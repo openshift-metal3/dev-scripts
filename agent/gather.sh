065795942544719fdcf40cab31285d5f78d8a975
Pawan Pinjarkar <ppinjark@redhat.com>
2022-12-06 09:38:22 -0600

    Shellcheck
    Signed-off-by: Pawan Pinjarkar <ppinjark@redhat.com>

--- agent/gather.sh
+++ agent/gather.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
 set -euxo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-source $SCRIPTDIR/agent/04_agent_configure.sh
+source "$SCRIPTDIR"/agent/04_agent_configure.sh
 
-ip=$(<${OCP_DIR}/node0-ip)
+ip=$(<"${OCP_DIR}"/node0-ip)
 
-ssh -o 'ConnectTimeout=30' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' core@${ip} if ! command -v /usr/local/bin/agent-gather '>&' /dev/null ';' then echo "Skipping gathering agent logs, agent-gather script not present." else sudo /usr/local/bin/agent-gather -o ';' fi >agent-gather.tar.xz
+ssh -o 'ConnectTimeout=30' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' core@"${ip}" if ! command -v /usr/local/bin/agent-gather '>&' /dev/null ';' then echo "Skipping gathering agent logs, agent-gather script not present." else sudo /usr/local/bin/agent-gather -o ';' fi >agent-gather.tar.xz
