1bcc06d6288439a43de8a1494d2b0b3e147c8156
Pawan Pinjarkar <ppinjark@redhat.com>
2022-12-06 00:17:53 -0600

    Code review fixes

--- agent/gather.sh
+++ agent/gather.sh
@@ -5,15 +5,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 source $SCRIPTDIR/agent/04_agent_configure.sh
 
-ip=$(head -n 1 ${OCP_DIR}/node0-ip)
+ip=$(<${OCP_DIR}/node0-ip)
 
-ssh -o 'ConnectTimeout=30' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' core@${ip} << EOF
-# check is agent-gather command is present
-if ! command -v /usr/local/bin/agent-gather &> /dev/null
-then
-    echo "Skipping gathering agent logs, agent-gather script not present."
-    exit 0
-fi
-
-sudo /usr/local/bin/agent-gather
-EOF
+ssh -o 'ConnectTimeout=30' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' core@${ip} if ! command -v /usr/local/bin/agent-gather '>&' /dev/null ';' then echo "Skipping gathering agent logs, agent-gather script not present." else sudo /usr/local/bin/agent-gather -o ';' fi >agent-gather.tar.xz
