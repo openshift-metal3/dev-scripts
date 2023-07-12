25b29df8959d01abacd9dc93c19cbcda077e97cf
Zane Bitter <zbitter@redhat.com>
2023-07-12 06:18:18 -0400

    Fix CVO overrides (#1537)
    Prior to OCP 4.10, the 'group' field in the override was ignored.
    Although "apps/v1" was the documented thing to pass, after
    openshift/cluster-version-operator#689 stopped ignoring the field, only
    "apps" works.
    
    My proposed fix for https://issues.redhat.com/browse/OCPBUGS-15390 is to
    make both work, so we might as well switch to the one that works with
    older releases.

--- start-mao.sh
+++ start-mao.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
 # Tell the cluster-version-operator to resume managing the machine-api-operator
-oc patch clusterversion version --namespace openshift-cluster-version --type merge -p '{"spec":{"overrides":[{"kind":"Deployment","group":"apps/v1","name":"machine-api-operator","namespace":"openshift-machine-api","unmanaged":false}]}}'
+oc patch clusterversion version --namespace openshift-cluster-version --type merge -p '{"spec":{"overrides":[{"kind":"Deployment","group":"apps","name":"machine-api-operator","namespace":"openshift-machine-api","unmanaged":false}]}}'
--- stop-mao.sh
+++ stop-mao.sh
@@ -15,7 +15,7 @@ else
 fi
 
 # Tell the cluster-version-operator to stop managing the machine-api-operator
-oc patch clusterversion version --namespace openshift-cluster-version --type merge -p '{"spec":{"overrides":[{"kind":"Deployment","group":"apps/v1","name":"machine-api-operator","namespace":"openshift-machine-api","unmanaged":true}]}}'
+oc patch clusterversion version --namespace openshift-cluster-version --type merge -p '{"spec":{"overrides":[{"kind":"Deployment","group":"apps","name":"machine-api-operator","namespace":"openshift-machine-api","unmanaged":true}]}}'
 
 # Stop any existing machine-api-operator
 oc scale deployment -n openshift-machine-api --replicas=0 machine-api-operator
