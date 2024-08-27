872f6b88c5eed15f49dfcf40df24cb1763b17d6e
Riccardo Pittau <elfosardo@users.noreply.github.com>
2024-08-27 16:05:00 +0200

    Move default release to 4.18 (#1696)
    This is valid for local runs only.

--- config_example.sh
+++ config_example.sh
@@ -31,10 +31,10 @@ set -x
 # OPENSHIFT_RELEASE_STREAM -
 # Select a different release stream from which to pull the latest image, if the
 # image name is not specified.
-# Default: 4.17
+# Default: 4.18
 # NOTE: Do not use for arm64, instead override OPENSHIFT_RELEASE_IMAGE
 #
-#export OPENSHIFT_RELEASE_STREAM=4.17
+#export OPENSHIFT_RELEASE_STREAM=4.18
 
 # OPENSHIFT_RELEASE_TYPE -
 # Select a different release type from which to pull the latest image.
