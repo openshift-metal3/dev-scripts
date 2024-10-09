2f1f26038238d059e13a567f725691e8807e2be8
Derek Higgins <derekh@redhat.com>
2024-10-09 15:21:48 +0100

    OCPBUGS-36506: Fix config for disconnected cluster
    Ensure all but BMC traffic goes via the proxy

--- network.sh
+++ network.sh
@@ -204,7 +204,10 @@ if  [[ ! -z "${INSTALLER_PROXY:-}" ]]; then
 
   # When a local registry is enabled (usually in disconnected environments), let's add it to the no proxy list
   if [[ ! -z "${MIRROR_IMAGES}" && "${MIRROR_IMAGES,,}" != "false" ]] || [[ ! -z "${ENABLE_LOCAL_REGISTRY}" ]]; then
-    NO_PROXY=$NO_PROXY,$LOCAL_REGISTRY_DNS_NAME
+    # If INSTALLER_PROXY has been set, all traffic must go via the proxy (the bm network has no access)
+    if [[ ${INSTALLER_PROXY:-false} == "false" ]]; then
+      NO_PROXY=$NO_PROXY,$LOCAL_REGISTRY_DNS_NAME
+    fi
   fi
 fi
 
