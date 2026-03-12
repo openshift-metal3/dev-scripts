9a5162cc202e7e3574d04372aa8975c1ec9da18c
Bob Fournier <bfournie@redhat.com>
2026-03-12 19:28:43 -0400

    OCPBUGS-77895: Allow for override of agent vcpu and memory (#1862)
    The CI conformance tests need more resources than currently
    configured for installations. Allow these settings to be
    overridden. This requires a corresponding change in
    openshift/release to use higher limits.

--- common.sh
+++ common.sh
@@ -288,26 +288,6 @@ export ARBITER_HOSTNAME_FORMAT=${ARBITER_HOSTNAME_FORMAT:-"arbiter-%d"}
 export WORKER_HOSTNAME_FORMAT=${WORKER_HOSTNAME_FORMAT:-"worker-%d"}
 export EXTRA_WORKER_HOSTNAME_FORMAT=${EXTRA_WORKER_HOSTNAME_FORMAT:-"extraworker-%d"}
 
-export MASTER_MEMORY=${MASTER_MEMORY:-16384}
-export MASTER_DISK=${MASTER_DISK:-60}
-export MASTER_VCPU=${MASTER_VCPU:-8}
-
-export ARBITER_MEMORY=${ARBITER_MEMORY:-8192}
-export ARBITER_DISK=${ARBITER_DISK:-50}
-export ARBITER_VCPU=${ARBITER_VCPU:-4}
-
-export WORKER_MEMORY=${WORKER_MEMORY:-8192}
-export WORKER_DISK=${WORKER_DISK:-60}
-export WORKER_VCPU=${WORKER_VCPU:-4}
-
-export EXTRA_WORKER_MEMORY=${EXTRA_WORKER_MEMORY:-${WORKER_MEMORY}}
-export EXTRA_WORKER_DISK=${EXTRA_WORKER_DISK:-${WORKER_DISK}}
-export EXTRA_WORKER_VCPU=${EXTRA_WORKER_VCPU:-${WORKER_VCPU}}
-
-export ARM_WORKER_MEMORY=${ARM_WORKER_MEMORY:-${WORKER_MEMORY}}
-export ARM_WORKER_DISK=${ARM_WORKER_DISK:-${WORKER_DISK}}
-export ARM_WORKER_VCPU=${ARM_WORKER_VCPU:-${WORKER_VCPU}}
-
 # Ironic vars (Image can be use <NAME>_LOCAL_IMAGE to override)
 export IRONIC_IMAGE=${IRONIC_IMAGE:-"quay.io/metal3-io/ironic:main"}
 export IRONIC_DATA_DIR="${WORKING_DIR}/ironic"
@@ -472,59 +452,59 @@ if [[ ! -z ${AGENT_E2E_TEST_SCENARIO} ]]; then
   case "$SCENARIO" in
       "5CONTROL" )
           export NUM_MASTERS=5
-          export MASTER_VCPU=4
-          export MASTER_DISK=100
-          export MASTER_MEMORY=24576
+          export MASTER_VCPU=${MASTER_VCPU:-4}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-24576}
           export NUM_WORKERS=0
           ;;
       "4CONTROL" )
           export NUM_MASTERS=4
-          export MASTER_VCPU=4
-          export MASTER_DISK=100
-          export MASTER_MEMORY=24576
+          export MASTER_VCPU=${MASTER_VCPU:-4}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-24576}
           export NUM_WORKERS=0
           ;;
       "COMPACT" )
           export NUM_MASTERS=3
-          export MASTER_VCPU=4
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-4}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_WORKERS=0
           ;;
       "TNA" )
           export NUM_MASTERS=2
-          export MASTER_VCPU=8
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-8}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_ARBITERS=1
-          export ARBITER_VCPU=2
-          export ARBITER_MEMORY=8192
-          export ARBITER_DISK=50
+          export ARBITER_VCPU=${ARBITER_VCPU:-2}
+          export ARBITER_MEMORY=${ARBITER_MEMORY:-8192}
+          export ARBITER_DISK=${ARBITER_DISK:-50}
           export NUM_WORKERS=0
           ;;
       "TNF" )
           export NUM_MASTERS=2
-          export MASTER_VCPU=8
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-8}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_WORKERS=0
           export ENABLE_TWO_NODE_FENCING="true"
           ;;
       "HA" )
           export NUM_MASTERS=3
-          export MASTER_VCPU=4
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-4}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_WORKERS=2
-          export WORKER_VCPU=4
-          export WORKER_DISK=100
-          export WORKER_MEMORY=9000
+          export WORKER_VCPU=${WORKER_VCPU:-4}
+          export WORKER_DISK=${WORKER_DISK:-100}
+          export WORKER_MEMORY=${WORKER_MEMORY:-9000}
           ;;
       "SNO" )
           export NUM_MASTERS=1
-          export MASTER_VCPU=8
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-8}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_WORKERS=0
           export NETWORK_TYPE="OVNKubernetes"
           export AGENT_PLATFORM_TYPE="${AGENT_PLATFORM_TYPE:-"none"}"
@@ -535,9 +515,9 @@ if [[ ! -z ${AGENT_E2E_TEST_SCENARIO} ]]; then
           ;;
       "SNOMIN" )
           export NUM_MASTERS=1
-          export MASTER_VCPU=4
-          export MASTER_DISK=100
-          export MASTER_MEMORY=32768
+          export MASTER_VCPU=${MASTER_VCPU:-4}
+          export MASTER_DISK=${MASTER_DISK:-100}
+          export MASTER_MEMORY=${MASTER_MEMORY:-32768}
           export NUM_WORKERS=0
           export NETWORK_TYPE="OVNKubernetes"
           export AGENT_PLATFORM_TYPE="${AGENT_PLATFORM_TYPE:-"none"}"
@@ -553,24 +533,34 @@ if [[ ! -z ${AGENT_E2E_TEST_SCENARIO} ]]; then
   # Increase master vCPU for agent OVE ISO installs or when certain operators like 'mtv' are used,
   # as some operators require more CPUs.
   if [ "${AGENT_E2E_TEST_BOOT_MODE}" == "ISO_NO_REGISTRY" ]; then
-    export MASTER_VCPU=9
+    if ((MASTER_VCPU < 9)); then
+      export MASTER_VCPU=9
+    fi
     if [ "${SCENARIO}" == "SNO" ]; then
-       export MASTER_VCPU=16
+       if ((MASTER_VCPU < 16)); then
+        export MASTER_VCPU=16
+      fi
     fi
     if [ "${SCENARIO}" == "HA" ]; then
-       export WORKER_VCPU=5
+       if ((WORKER_VCPU < 5)); then
+        export WORKER_VCPU=5
+      fi
     fi
     # Increase disk storage requirements for NoRegistryClusterInstall aka agent OVE ISO
     # Large storage size is used to catch any regression related to https://issues.redhat.com/browse/OCPBUGS-76382.
     case "$SCENARIO" in
       "SNO"|"COMPACT"|"HA" )
-        export MASTER_DISK=300
+          if ((MASTER_DISK < 300)); then
+            export MASTER_DISK=300
+          fi
         ;;
     esac
   fi
 
   if [[ "$AGENT_OPERATORS" =~ "mtv" ]]; then
-    export MASTER_VCPU=9
+    if ((MASTER_VCPU < 9)); then
+      export MASTER_VCPU=9
+    fi
   fi
 
   if [ ! -z "${AGENT_DEPLOY_MCE}" ]; then
@@ -579,8 +569,12 @@ if [[ ! -z ${AGENT_E2E_TEST_SCENARIO} ]]; then
     export VM_EXTRADISKS_LIST="vda vdb"
     export VM_EXTRADISKS_SIZE="10G"
 
-    export MASTER_VCPU=8
-    export MASTER_MEMORY=32768
+    if ((MASTER_VCPU < 8)); then
+      export MASTER_VCPU=8
+    fi
+    if ((MASTER_MEMORY < 32768)); then
+      export MASTER_MEMORY=32768
+    fi
   fi
 
   if [[ $IP_STACK != 'v4' ]] && [[ $IP_STACK != 'v6' ]] && [[ $IP_STACK != 'v4v6' ]]; then
@@ -645,6 +639,25 @@ fi
 
 export AGENT_TEST_CASES=${AGENT_TEST_CASES:-}
 
+export MASTER_MEMORY=${MASTER_MEMORY:-16384}
+export MASTER_DISK=${MASTER_DISK:-60}
+export MASTER_VCPU=${MASTER_VCPU:-8}
+
+export ARBITER_MEMORY=${ARBITER_MEMORY:-8192}
+export ARBITER_DISK=${ARBITER_DISK:-50}
+export ARBITER_VCPU=${ARBITER_VCPU:-4}
+
+export WORKER_MEMORY=${WORKER_MEMORY:-8192}
+export WORKER_DISK=${WORKER_DISK:-60}
+export WORKER_VCPU=${WORKER_VCPU:-4}
+
+export EXTRA_WORKER_MEMORY=${EXTRA_WORKER_MEMORY:-${WORKER_MEMORY}}
+export EXTRA_WORKER_DISK=${EXTRA_WORKER_DISK:-${WORKER_DISK}}
+export EXTRA_WORKER_VCPU=${EXTRA_WORKER_VCPU:-${WORKER_VCPU}}
+
+export ARM_WORKER_MEMORY=${ARM_WORKER_MEMORY:-${WORKER_MEMORY}}
+export ARM_WORKER_DISK=${ARM_WORKER_DISK:-${WORKER_DISK}}
+export ARM_WORKER_VCPU=${ARM_WORKER_VCPU:-${WORKER_VCPU}}
 
 export PERSISTENT_IMAGEREG=${PERSISTENT_IMAGEREG:-false}
 if [ "${OPENSHIFT_CI}" == true ] ; then
