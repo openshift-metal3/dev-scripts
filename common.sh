6cf59b237e46bdda3f9291713e5654515c6b5a52
Francesco Cappa <24370687+fracappa@users.noreply.github.com>
2026-04-22 14:10:09 +0200

    NO-JIRA: Set customizable TNA NUM_WORKERS in common.sh (#1869)
    * feat: changing NUM_WORKERS env var in TNA to be customizable
    
    This way TNA clusters with day-0 workers could be installed using dev-scripts
    
    * feat: changing NUM_WORKERS env vars in TNF to be customizable
    
    ---------
    
    Co-authored-by: Francesco Cappa <fcappa@fcappa-thinkpadp1gen7.rmtit.csb>

--- common.sh
+++ common.sh
@@ -480,14 +480,14 @@ if [[ ! -z ${AGENT_E2E_TEST_SCENARIO} ]]; then
           export ARBITER_VCPU=${ARBITER_VCPU:-2}
           export ARBITER_MEMORY=${ARBITER_MEMORY:-8192}
           export ARBITER_DISK=${ARBITER_DISK:-50}
-          export NUM_WORKERS=0
+          export NUM_WORKERS=${NUM_WORKERS:-0}
           ;;
       "TNF" )
           export NUM_MASTERS=2
           export MASTER_VCPU=${MASTER_VCPU:-8}
           export MASTER_DISK=${MASTER_DISK:-100}
           export MASTER_MEMORY=${MASTER_MEMORY:-32768}
-          export NUM_WORKERS=0
+          export NUM_WORKERS=${NUM_WORKERS:-0}
           export ENABLE_TWO_NODE_FENCING="true"
           ;;
       "HA" )
